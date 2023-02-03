0fc606a6dd55231c9428958f9eb45ebd7f3b4657
MarianoANDR <mariano.uk2022@gmail.com>
2023-02-03 20:35:44 +0000

    development branch created

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 
-const PORT = 5000;
+const PORT = 5000; 
 
 app.listen(PORT, (req, res) => {
     console.log(`Express Server running on port ${PORT}!`)
