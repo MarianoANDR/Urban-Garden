60bebaefcefdbf0bce85d2ac19bb2a073d9ad976
MarianoANDR <mariano.uk2022@gmail.com>
2023-03-17 16:26:24 +0000

    clean up code

--- server/server.js
+++ server/server.js
@@ -1,14 +1,11 @@
 import express from "express";
-import mongoose from "mongoose";
 import * as dotenv from "dotenv";
 import { usersRouter } from "./Routes/UsersRoutes.js";
+import { vegetablesRouter } from "./Routes/VegetablesRoutes.js";
+import { locationRouter } from "./Routes/LocationRoutes.js";
 import { errorHandle } from "./Middeware/ErrorMiddleware.js";
 import { connectDB } from "./config/db.js";
-import { vegetablesRouter } from "./Routes/VegetablesRoutes.js";
-import axios from "axios";
-import asyncHandler from "express-async-handler";
 import requestIp from "request-ip";
-import { locationRouter } from "./Routes/LocationRoutes.js";
 
 const app = express();
 dotenv.config();
@@ -19,10 +16,9 @@ app.use(express.urlencoded({ extended: false })); //middleware & access req.body
 
 const PORT = 4000;
 
-
 app.use("/users", usersRouter);
-app.use("/vegetables", vegetablesRouter);
 app.use("/location", locationRouter);
+app.use("/vegetables", vegetablesRouter);
 
 app.use(requestIp.mw());
 app.use(errorHandle);
